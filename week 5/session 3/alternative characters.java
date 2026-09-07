168f02c3264fbf996955f89cb93b4e57beaba24a
ALAVALA-JITHIN <vtu27896@veltech.edu.in>
2026-09-07 11:12:07 +0530

    Create alternative characters.java

---
+++ week 5/session 3/alternative characters.java
@@ -0,0 +1,21 @@
+public static int alternatingCharacters(String s) {
+
+    int count = 0;
+
+    for (int i = 1; i < s.length(); i++) {
+
+        if (s.charAt(i) == s.charAt(i - 1)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+//output:
+Input:
+AABAAB
+
+Output:
+2

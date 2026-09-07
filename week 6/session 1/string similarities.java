acc7faed470a8c4ac2641a2e32a7e5813e635f50
ALAVALA-JITHIN <vtu27896@veltech.edu.in>
2026-09-07 11:27:07 +0530

    Create string similarities.java

---
+++ week 6/session 1/string similarities.java
@@ -0,0 +1,24 @@
+public static long stringSimilarity(String s) {
+
+    long total = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        int j = 0;
+
+        while (i + j < s.length()
+                && s.charAt(j) == s.charAt(i + j)) {
+            j++;
+        }
+
+        total += j;
+    }
+
+    return total;
+}
+//output:
+Input:
+ababaa
+
+Output:
+11

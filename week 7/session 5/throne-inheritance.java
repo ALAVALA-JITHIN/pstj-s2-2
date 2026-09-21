422c29da4bb09c5fc0007f9181e86bc7b6a2d686
ALAVALA-JITHIN <vtu27896@veltech.edu.in>
2026-09-21 11:56:13 +0530

    Add ThroneInheritance class with basic functionality
    Implement ThroneInheritance class with methods for birth, death, and inheritance order retrieval.

---
+++ week 7/session 5/throne-inheritance.java
@@ -0,0 +1,46 @@
+```java
+import java.util.*;
+
+class ThroneInheritance {
+
+    String king;
+    HashMap<String, List<String>> children;
+    HashSet<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        children = new HashMap<>();
+        dead = new HashSet<>();
+
+        children.put(kingName, new ArrayList<>());
+    }
+
+    public void birth(String parentName, String childName) {
+
+        children.putIfAbsent(parentName, new ArrayList<>());
+
+        children.get(parentName).add(childName);
+
+        children.put(childName, new ArrayList<>());
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+
+        List<String> result = new ArrayList<>();
+
+        dfs(king, result);
+
+        return result;
+    }
+
+    private void dfs(String person, List<String> result) {
+
+        // Add person if they are alive
+        if (!dead.contains(person)) {
+            result.add(person);
+        }
+```

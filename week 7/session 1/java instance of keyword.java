1828dcf93abd81694e84f4b8cf9b4c0d1325574a
ALAVALA-JITHIN <vtu27896@veltech.edu.in>
2026-09-21 11:38:28 +0530

    Create java instance of keyword.java

---
+++ week 7/session 1/java instance of keyword.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+
+    public int divisor_sum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        return sum;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        MyCalculator myCalculator = new MyCalculator();
+
+        System.out.print("I implemented: AdvancedArithmetic");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(myCalculator.divisor_sum(n));
+    }
+}
+
